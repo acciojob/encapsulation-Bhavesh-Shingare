56b09af9a53eb700da886a89db3b1ceae6fc1a6e
Bhavesh <you@example.com>
2023-02-10 14:06:18 +0530

    encap assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
+    public static void main(String[] args){
+        RWOnly obj= new RWOnly();
+
+        obj.setName("Bhavesh");
+
+        System.out.println(obj.getName());
+
+    }
   
 }
